bf480caf815b418b9e6aa223e2e865784e3f0be2
unknown <dienreus1406@gmail.com>
2025-09-29 00:41:53 +0700

    add

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,10 @@ import { Like } from './domain/entities/like.entity';
 import { UserResolver } from './presentation/graphql/resolvers/user.resolver';
 import { PostResolver } from './presentation/graphql/resolvers/post.resolver';
 import { CommentResolver } from './presentation/graphql/resolvers/comment.resolver';
+import { UserModule } from './modules/user.module';
+import { PostModule } from './modules/post.module';
+import { CommentModule } from './modules/comment.module';
+import { LikeModule } from './modules/like.module';
 
 @Module({
   imports: [
@@ -18,16 +22,21 @@ import { CommentResolver } from './presentation/graphql/resolvers/comment.resolv
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: 'dpg-d340b5nfte5s73ebqi6g-a.oregon-postgres.render.com',
       port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'nestdb',
+      username: 'veo3_api_user',
+      password: 'WuIvkqeoiGlH2HPwxCfqju7iabdKnWD2',
+      database: 'veo3_api',
+      schema: 'fb-test',
       entities: [User, Post, Comment, Like],
       synchronize: true,
     }),
+    UserModule,
+    PostModule,
+    CommentModule,
+    LikeModule,
     TypeOrmModule.forFeature([User, Post, Comment, Like]),
   ],
   providers: [UserResolver, PostResolver, CommentResolver],
 })
-export class AppModule {}
+export class AppModule { }
